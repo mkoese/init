81109740c2fd54851ed802bf04a303bd16fd2bc4
Mikail Koese <mikail.koese@gmail.com>
2023-07-16 12:06:35 +0200

    Fix sync scripts

--- sync-cloud-to-home.sh
+++ sync-cloud-to-home.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 #export RCLONE_CONFIG_PASS='changeit'
-
-rclone copyto dropbox:sync-cloud-to-home.sh sync-cloud-to-home.sh
-rclone copyto dropbox:sync-home-to-cloud.sh sync-home-to-cloud.sh
-
 rclone sync dropbox-sealed:/Sealed/ Secrets/ -uv --progress --exclude ".**"
 
 FOLDER_NAME=Documents
-rclone sync dropbox:/$FOLDER_NAME/ $FOLDER_NAME/ -uv --progress --exclude ".**"
+rclone sync dropbox:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -uv --progress --exclude ".**"
 
 FOLDER_NAME=Pictures
-rclone sync dropbox:/$FOLDER_NAME/ -uv $FOLDER_NAME/ --progress --exclude ".**"
+rclone sync dropbox:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -uv --progress --exclude ".**"
 
