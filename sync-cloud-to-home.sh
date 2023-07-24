aa5f12f63a22bc530ac3028677476001331e5659
Mikail Koese <mikail.koese@gmail.com>
2023-07-24 21:45:55 +0200

    Do the Alfred trick

--- sync-cloud-to-home.sh
+++ sync-cloud-to-home.sh
@@ -8,7 +8,8 @@ FOLDER_NAME=Secrets
 rclone sync $BACKUP_SOURCE-sealed:/Sealed/ ~/$FOLDER_NAME/ -uP --exclude ".**"
 
 FOLDER_NAME=Documents
-rclone sync $BACKUP_SOURCE:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -uP --exclude ".**"
+rclone sync $BACKUP_SOURCE:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -uP --exclude ".**" --exclude "*.alfredpreferences"
+tar -xJvpf ~/Documents/Preferences/Alfred.tar.xz -C ~/Documents/Preferences/
 
 FOLDER_NAME=Pictures
 rclone sync $BACKUP_SOURCE:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -uP --exclude ".**"
--- sync-home-to-usb.sh
+++ sync-home-to-usb.sh
@@ -6,7 +6,8 @@ rsync ~/$FOLDER_NAME/ /mnt/usb-sealed/$FOLDER_NAME/ -avP --exclude ".**"
 
 FOLDER_NAME=Documents
 echo "Upload ${FOLDER_NAME}"
-rsync ~/$FOLDER_NAME/ /mnt/usb-sealed/$FOLDER_NAME/ -avP --exclude ".**"
+rsync ~/$FOLDER_NAME/ /mnt/usb-sealed/$FOLDER_NAME/ -avP --exclude ".**" --exclude "*.alfredpreferences"
+tar -xJvpf ~/Documents/Preferences/Alfred.tar.xz -C ~/Documents/Preferences/
 
 FOLDER_NAME=Pictures
 echo "Upload ${FOLDER_NAME}"
