51b6ea61fd08b0c569b8b680bdcf0751b6708bb2
Mikail Koese <mikail.koese@gmail.com>
2023-07-24 22:27:46 +0200

    Fix home-to-usb script

--- sync-home-to-usb.sh
+++ sync-home-to-usb.sh
@@ -5,10 +5,10 @@ echo "Write Secrets"
 rsync ~/$FOLDER_NAME/ /mnt/usb-sealed/$FOLDER_NAME/ -avP --exclude ".**"
 
 FOLDER_NAME=Documents
-echo "Upload ${FOLDER_NAME}"
+echo "Write ${FOLDER_NAME}"
+tar --mac-metadata -cvpf ~/Documents/Preferences/Alfred.tar.xz ~/Documents/Preferences/Alfred.alfredpreferences/
 rsync ~/$FOLDER_NAME/ /mnt/usb-sealed/$FOLDER_NAME/ -avP --exclude ".**" --exclude "*.alfredpreferences/"
-tar -xJvpf ~/Documents/Preferences/Alfred.tar.xz -C ~/Documents/Preferences/
 
 FOLDER_NAME=Pictures
-echo "Upload ${FOLDER_NAME}"
+echo "Write ${FOLDER_NAME}"
 rsync ~/$FOLDER_NAME/ /mnt/usb-sealed/$FOLDER_NAME/ -avP --exclude ".**"
