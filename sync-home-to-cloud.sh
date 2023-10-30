7a358c37f272e9557d3dad6f7b92f1085ba04f18
Mikail Koese <mikail.koese@gmail.com>
2023-10-30 21:40:22 +0100

    Extend backup script with copy and syncå

--- sync-home-to-cloud.sh
+++ sync-home-to-cloud.sh
@@ -2,26 +2,44 @@
 
 OS=$(uname)
 
+cd ~
 #export RCLONE_CONFIG_PASS='changeit'
 FOLDER_NAME=Secrets
-echo "Upload Secrets"
-rclone sync $FOLDER_NAME/ dropbox-sealed:/Sealed/ -uP --exclude ".**"
+echo "Upload ${FOLDER_NAME}"
+rclone copy $FOLDER_NAME/ dropbox-sealed:/Sealed/ -uP --exclude ".**"
+
+echo "Download ${FOLDER_NAME} from Cloud to Local"
+rclone sync dropbox-sealed:/Sealed/ $FOLDER_NAME/  -uP --exclude ".**"
+
 
 if [ "$OS" == "Darwin" ]; then
     echo "Running on macOS"
     cd ~/Documents/Preferences
-    tar --mac-metadata -cvpf Alfred.tar.xz Alfred.alfredpreferences/
+    tar --mac-metadata -cpJf Alfred.tar.xz Alfred.alfredpreferences/
     cd ~
 fi
 
 FOLDER_NAME=Documents
 echo "Upload ${FOLDER_NAME}"
-rclone sync ~/$FOLDER_NAME/ dropbox:/$FOLDER_NAME/ -uP --exclude ".**" --exclude "*.alfredpreferences/"
-rclone sync ~/$FOLDER_NAME/ onedrive:/$FOLDER_NAME/ -uP --exclude ".**" --exclude "*.alfredpreferences/"
-rclone sync ~/$FOLDER_NAME/ gdrive:/$FOLDER_NAME/ -uP --exclude ".**" --exclude "*.alfredpreferences/"
+rclone copy ~/$FOLDER_NAME/ dropbox:/$FOLDER_NAME/ -u --exclude ".**" --exclude "*.alfredpreferences/"
+rclone copy ~/$FOLDER_NAME/ onedrive:/$FOLDER_NAME/ -u --exclude ".**" --exclude "*.alfredpreferences/"
+rclone copy ~/$FOLDER_NAME/ gdrive:/$FOLDER_NAME/ -u --exclude ".**" --exclude "*.alfredpreferences/"
+
+echo "Download ${FOLDER_NAME} from Cloud to Local"
+rclone sync dropbox:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -u --exclude ".**" --exclude "*.alfredpreferences/"
+rclone sync onedrive:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -u --exclude ".**" --exclude "*.alfredpreferences/"
+rclone sync gdrive:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -u --exclude ".**" --exclude "*.alfredpreferences/"
+
 
 FOLDER_NAME=Pictures
 echo "Upload ${FOLDER_NAME}"
-rclone sync ~/$FOLDER_NAME/ dropbox:/$FOLDER_NAME/ -uP --exclude ".**"
-rclone sync ~/$FOLDER_NAME/ onedrive:/$FOLDER_NAME/ -uP --exclude ".**"
-rclone sync ~/$FOLDER_NAME/ gdrive:/$FOLDER_NAME/ -uP --exclude ".**"
+rclone copy ~/$FOLDER_NAME/ dropbox:/$FOLDER_NAME/ -u --exclude ".**"
+rclone copy ~/$FOLDER_NAME/ onedrive:/$FOLDER_NAME/ -u --exclude ".**"
+rclone copy ~/$FOLDER_NAME/ gdrive:/$FOLDER_NAME/ -u --exclude ".**"
+
+echo "Download ${FOLDER_NAME} from Cloud to Local"
+rclone sync dropbox:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -u --exclude ".**"
+rclone sync onedrive:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -u --exclude ".**"
+rclone sync gdrive:/$FOLDER_NAME/ ~/$FOLDER_NAME/ -u --exclude ".**"
+
+echo "Done"
